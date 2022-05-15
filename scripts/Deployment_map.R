5ffe2bc1a299d80cd0989bf02ab32d409efe3066
leahcrowe-otago <leah.crowe@postgrad.otago.ac.nz>
2022-05-15 19:01:53 +1200

    round position to 5 digits, write html file

--- scripts/Deployment_map.R
+++ scripts/Deployment_map.R
@@ -7,9 +7,11 @@ natpark<-subset(protected_areas, (section == "s.4 - National Park"))
 mpa<-subset(protected_areas, (section == "s.3 - Marine Reserve"))
 
 deployments<-read.csv('./data/Deployment locations.csv', header = T, stringsAsFactors = T)
+deployments$Latitude<-round(deployments$Latitude,5)
+deployments$Longitude<-round(deployments$Longitude,5)
 
 factpal <- colorFactor(palette = "Dark2", domain = deployments$PROJECT)
-leaflet(data = deployments) %>% 
+deploy_leaf<-leaflet(data = deployments) %>% 
   addEsriBasemapLayer(esriBasemapLayers$Oceans, autoLabels=TRUE)%>%
   addPolygons(data = mpa, color = "orange", weight = 1) %>%
   addCircleMarkers(lng = ~deployments$Longitude, lat = ~deployments$Latitude, color = ~factpal(deployments$PROJECT), stroke = FALSE, fillOpacity = 2, radius = 5,
@@ -17,3 +19,6 @@ leaflet(data = deployments) %>%
   addLegend(pal = factpal, values = deployments$PROJECT, opacity = 1, title = "Project")%>%
   addLegend(colors = "orange", labels = "Marine Reserves")%>%
   fitBounds(min(deployments$Longitude), min(deployments$Latitude), max(deployments$Longitude), max(deployments$Latitude))
+
+library(htmlwidgets)
+saveWidget(deploy_leaf, file="index.html")
