781b03c110ed1bd59dd5ed4359c6bb6b166da768
leahcrowe-otago <leah.crowe@postgrad.otago.ac.nz>
2022-11-03 17:04:59 +1300

    update with sp class and qn

--- scripts/FPOD.R
+++ scripts/FPOD.R
@@ -1,43 +1,68 @@
-library(ggplot2);library(lubridate);library(dplyr);library(readxl)
+library(ggplot2);library(lubridate);library(dplyr);library(readxl);library(viridis)
 
 deploy<-read_excel("./data/Fiordland deployment locations.xlsx")
 deploy<-deploy%>%filter(Fiord != "DUSKY")
 
-
-pres01_01<-read.delim('./data/FPOD data/Pres01_01 DPM day.txt', header = T, sep ="\t", quote = "\"",dec = ".", skip = 9, row.names = NULL)
-pres01_02<-read.delim('./data/FPOD data/Pres01_02 DPM day.txt', header = T, sep ="\t", quote = "\"",dec = ".", skip = 9, row.names = NULL)
-charles01_01<-read.delim('./data/FPOD data/Charles01_01 DPM day.txt', header = T, sep ="\t", quote = "\"",dec = ".", skip = 10, row.names = NULL)
-charles01_02<-read.delim('./data/FPOD data/Charles01_02 DPM day.txt', header = T, sep ="\t", quote = "\"",dec = ".", skip = 9, row.names = NULL)
-dagg01_01<-read.delim('./data/FPOD data/Dagg01_01 DPM day.txt', header = T, sep ="\t", quote = "\"",dec = ".", skip = 10, row.names = NULL)
+charles01_01<-read.delim('./data/FPOD data/Charles0101 2022 02 15 FPOD_6192 file0 train details.txt', header = T, sep ="\t", quote = "\"",dec = ".", skip = 40, row.names = NULL)
+charles01_02<-read.delim('./data/FPOD data/Charles0102 2022 05 06 FPOD_6192 file0 train details.txt', header = T, sep ="\t", quote = "\"",dec = ".", skip = 40, row.names = NULL)
+#double check this was run without the NBHF kerno filter
+dagg01_01<-read.delim('./data/FPOD data/Dagg0101 2022 02 16 FPOD_6194 file0 train details.txt', header = T, sep ="\t", quote = "\"",dec = ".", skip = 40, row.names = NULL)
 dagg01_02<-read.delim('./data/FPOD data/Dagg01_02 DPM day.txt', header = T, sep ="\t", quote = "\"",dec = ".", skip = 10, row.names = NULL)
+#double check this was run without the NBHF kerno filter
+pres01_01<-read.delim('./data/FPOD data/Preservation0101 2022 02 21 FPOD_6190 file0 train details.txt', header = T, sep ="\t", quote = "\"",dec = ".", skip = 40, row.names = NULL)
+pres01_02<-read.delim('./data/FPOD data/Pres01_02 DPM day.txt', header = T, sep ="\t", quote = "\"",dec = ".", skip = 9, row.names = NULL)
 
 charles01<-charles01_01%>%
   bind_rows(charles01_02)%>%
   mutate(Fiord = "CHARLES")
 
 dagg01<-dagg01_01%>%
-  bind_rows(dagg01_02)%>%
+  #bind_rows(dagg01_02)%>%
   mutate(Fiord = "DAGG")
 
 pres01<-pres01_01%>%
-  bind_rows(pres01_02)%>%
+  #bind_rows(pres01_02)%>%
   mutate(Fiord = "PRESERVATION")
 
 all<-charles01%>%
   bind_rows(dagg01)%>%
   bind_rows(pres01)%>%
-  mutate(Date = as.Date(ChunkEnd))%>%
-  group_by(Date, Fiord)%>%
-  mutate(DPM = sum(DPM))%>%
-  mutate(presence = case_when(
-    DPM > 0 ~ 1,
-    DPM == 0 ~ 0
-  ))
+  mutate(Date = as.Date(dmy_hm(Time)))%>%
+  filter(Qn == "High" | Qn == "Mod")
 
-ggplot(all)+
-  geom_col(aes(x = Date, y = presence, fill = Fiord))+
+all$Qn<-factor(all$Qn, levels = c("Mod","High"))
+
+all_Tt<-all%>%  
+  filter(SpClass == "Other cet")%>%
+  group_by(Date, Fiord, Qn)%>%
+  mutate(DPD = n())%>% #DPD = detections per day
+  distinct(Date, Fiord, SpClass, DPD, Qn)
+  # mutate(presence = case_when(
+  #   DPM > 0 ~ 1,
+  #   DPM == 0 ~ 0
+  # ))
+
+
+
+ggplot(all_Tt)+
+  geom_col(aes(x = Date, y = 1, fill = Qn))+
   facet_wrap(~Fiord, ncol = 1)+
   ylim(c(0,1))+
   theme_bw()+
-  theme(legend.position = "none")+
-  geom_vline(deploy, mapping = aes(xintercept = as.Date(deploy$Datetime_deployment)))
+  scale_fill_brewer(palette = "Paired")+
+  geom_vline(deploy, mapping = aes(xintercept = as.Date(Datetime_deployment)))+
+  ylab("")
+
+all_NBHF<-all%>%
+  filter(SpClass == "NBHF")%>%
+  group_by(Date, Fiord, Qn)%>%
+  mutate(DPD = n())%>% #DPD = detections per day
+  distinct(Date, Fiord, SpClass, DPD, Qn)
+
+ggplot(all_NBHF)+
+  geom_col(aes(x = Date, y = 1, fill = Qn))+
+  facet_wrap(~Fiord, ncol = 1)+
+  ylim(c(0,1))+
+  theme_bw()+
+  scale_fill_brewer(palette = "Paired")+
+  geom_vline(deploy, mapping = aes(xintercept = as.Date(Datetime_deployment)))
