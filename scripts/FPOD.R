c608c41e26bf886a77790df28b359d4e77d96f1b
leahcrowe-otago <leah.crowe@postgrad.otago.ac.nz>
2023-05-12 16:47:49 +1200

    v1

--- scripts/FPOD.R
+++ scripts/FPOD.R
@@ -40,7 +40,7 @@ nbhf_day%>%
   group_by(Date)%>%
   tally()
 
-write.csv(nbhf_time, paste0('./data/Fiordland_nbhf_', Sys.Date(),'.csv'), row.names = F)
+write.csv(nbhf_time, paste0('./data/Fiordland_nbhf_', Sys.Date(),'_v1.csv'), row.names = F)
 
 acou_timeline<-function(x){
   ggplot(x)+
@@ -61,24 +61,26 @@ all_Cet_plot<-acou_timeline(all_Cet)+
   facet_wrap(~factor(Fiord, levels = c("CHARLES","NANCY","DAGG","CHALKY","PRESERVATION")), ncol = 1)+
   geom_vline(deploy, mapping = aes(xintercept = as.Date(Datetime_deployment)), linetype = "twodash", color = "red")
 
-ggsave('./figures/allcet.png',all_Cet_plot, dpi = 300, width = 175, height = 125, units = "mm")
+all_Cet_plot
+
+ggsave('./figures/allcet_v1.png',all_Cet_plot, dpi = 300, width = 175, height = 125, units = "mm")
 
 all_NBHF_plot<-acou_timeline(nbhf_day)+
   facet_wrap(~factor(Fiord, levels = c("CHARLES","DAGG","PRESERVATION")), ncol = 1)+
   geom_vline(deploy%>%filter(Recorder_type == 'F-POD'), mapping = aes(xintercept = as.Date(Datetime_deployment)), linetype = "twodash", color = "red")
 
-ggsave('./figures/NBHF.png', all_NBHF_plot, dpi = 300, width = 175, height = 125, units = "mm")
+ggsave('./figures/NBHF_v1.png', all_NBHF_plot, dpi = 300, width = 175, height = 125, units = "mm")
 
-   
 all_Cet_plot+
   #soundtrap died shaded areas 
   geom_rect(data = data.frame(Fiord = "NANCY"), aes(xmin = ymd("2022-10-07"), xmax = ymd("2022-11-27"), ymin = 0, ymax = 1), fill="grey", alpha = 0.5, inherit.aes = FALSE)+
+  geom_rect(data = data.frame(Fiord = "NANCY"), aes(xmin = ymd("2023-01-02"), xmax = ymd("2023-03-14"), ymin = 0, ymax = 1), fill="grey", alpha = 0.5, inherit.aes = FALSE)+
   geom_rect(data = data.frame(Fiord = "DAGG"), aes(xmin = ymd("2022-11-15"), xmax = ymd("2022-11-27"), ymin = 0, ymax = 1), fill="grey", alpha = 0.5, inherit.aes = FALSE)+
   geom_rect(data = data.frame(Fiord = "CHALKY"), aes(xmin = ymd("2022-11-16"), xmax = ymd("2023-04-28"), ymin = 0, ymax = 1), fill="grey", alpha = 0.5, inherit.aes = FALSE)+
   #FPOD died
   geom_rect(data = data.frame(Fiord = "PRESERVATION"), aes(xmin = ymd("2023-03-15"), xmax = ymd("2023-04-28"), ymin = 0, ymax = 1), fill="grey", alpha = 0.5, inherit.aes = FALSE)
 
-trunc_nbhf_othercet<-ggplot(all%>%filter(SpClass == "NBHF" | SpClass == "Other cet"))+
+trunc_nbhf_othercet<-ggplot(all%>%filter(SpClass == "NBHF" | SpClass == "Dol"))+
   geom_col(aes(x = Date, y = 1, fill = Qn))+
   facet_wrap(~SpClass+Fiord, ncol = 3)+
   ylim(c(0,1))+
@@ -91,8 +93,14 @@ trunc_nbhf_othercet<-ggplot(all%>%filter(SpClass == "NBHF" | SpClass == "Other c
         panel.grid.minor.y = element_blank(),
         panel.grid.major.y = element_blank())
 
-ggsave('./figures/trunc_nbhf_othercet.png', trunc_nbhf_othercet, dpi = 300, width = 300, height = 80, units = "mm")
+ggsave('./figures/trunc_nbhf_othercet_v1.png', trunc_nbhf_othercet, dpi = 300, width = 300, height = 80, units = "mm")
 
 locations_FPOD<-deploy%>%filter(Recorder_type == 'F-POD' & !is.na(Recorder_start))
 
 write.csv(locations_FPOD, paste0('./data/Fiordland_FPOD_locations_', Sys.Date(),'.csv'), row.names = F)
+
+#all things
+acou_timeline(all)+
+  facet_wrap(~factor(Fiord, levels = c("CHARLES","DAGG","PRESERVATION")), ncol = 1)+
+  facet_wrap(~factor(Fiord, levels = c("CHARLES","NANCY","DAGG","CHALKY","PRESERVATION")), ncol = 1)+
+  geom_vline(deploy, mapping = aes(xintercept = as.Date(Datetime_deployment)), linetype = "twodash", color = "red")
