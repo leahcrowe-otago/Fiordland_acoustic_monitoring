a02905cf6bfb3f9917ecd244df65c9ebe33e86fe
leahcrowe-otago <leah.crowe@postgrad.otago.ac.nz>
2023-06-07 09:14:39 +1200

    update with charles 01_04

--- scripts/FPOD.R
+++ scripts/FPOD.R
@@ -13,7 +13,8 @@ FPOD_data<-lapply(FPOD_data_list, function(x)
 all<-bind_rows(FPOD_data)%>%
   mutate(Fiord = toupper(stringr::str_extract(File, '^[^01]+')))%>%
   mutate(Datetime = ymd_hm("1899-12-30 00:00") + minutes(Min),
-         Date = as.Date(Datetime))
+         Date = as.Date(Datetime))%>%
+  filter(!(grepl("Charles0104", File) & Datetime >= ymd_hms("2023-05-13 15:44:00")))
 
 unique(all$Qn)
 
@@ -49,7 +50,7 @@ acou_timeline<-function(x){
   theme_bw()+
   scale_fill_brewer(palette = "Paired")+
   ylab("")+
-  xlim(c(min(as.Date(deploy$Datetime_deployment), na.rm = T), max(as.Date(deploy$Datetime_deployment), na.rm = T)))+
+  xlim(c(min(as.Date(deploy$Datetime_deployment_local), na.rm = T), max(as.Date(deploy$Datetime_deployment_local), na.rm = T)))+
   theme(axis.text.y=element_blank(),  #remove y axis labels
         axis.ticks.y=element_blank(),
         panel.grid.minor.y = element_blank(),
@@ -59,7 +60,7 @@ acou_timeline<-function(x){
 
 all_Cet_plot<-acou_timeline(all_Cet)+
   facet_wrap(~factor(Fiord, levels = c("CHARLES","NANCY","DAGG","CHALKY","PRESERVATION")), ncol = 1)+
-  geom_vline(deploy, mapping = aes(xintercept = as.Date(Datetime_deployment)), linetype = "twodash", color = "red")
+  geom_vline(deploy, mapping = aes(xintercept = as.Date(Datetime_deployment_local)), linetype = "twodash", color = "red")
 
 all_Cet_plot
 
@@ -67,7 +68,7 @@ ggsave('./figures/allcet_v1.png',all_Cet_plot, dpi = 300, width = 175, height = 
 
 all_NBHF_plot<-acou_timeline(nbhf_day)+
   facet_wrap(~factor(Fiord, levels = c("CHARLES","DAGG","PRESERVATION")), ncol = 1)+
-  geom_vline(deploy%>%filter(Recorder_type == 'F-POD'), mapping = aes(xintercept = as.Date(Datetime_deployment)), linetype = "twodash", color = "red")
+  geom_vline(deploy%>%filter(Recorder_type == 'F-POD'), mapping = aes(xintercept = as.Date(Datetime_deployment_local)), linetype = "twodash", color = "red")
 
 ggsave('./figures/NBHF_v1.png', all_NBHF_plot, dpi = 300, width = 175, height = 125, units = "mm")
 
@@ -95,7 +96,7 @@ trunc_nbhf_othercet<-ggplot(all%>%filter(SpClass == "NBHF" | SpClass == "Dol"))+
 
 ggsave('./figures/trunc_nbhf_othercet_v1.png', trunc_nbhf_othercet, dpi = 300, width = 300, height = 80, units = "mm")
 
-locations_FPOD<-deploy%>%filter(Recorder_type == 'F-POD' & !is.na(Recorder_start))
+locations_FPOD<-deploy%>%filter(Recorder_type == 'F-POD' & !is.na(Recorder_start_local))
 
 write.csv(locations_FPOD, paste0('./data/Fiordland_FPOD_locations_', Sys.Date(),'.csv'), row.names = F)
 
@@ -103,4 +104,4 @@ write.csv(locations_FPOD, paste0('./data/Fiordland_FPOD_locations_', Sys.Date(),
 acou_timeline(all)+
   facet_wrap(~factor(Fiord, levels = c("CHARLES","DAGG","PRESERVATION")), ncol = 1)+
   facet_wrap(~factor(Fiord, levels = c("CHARLES","NANCY","DAGG","CHALKY","PRESERVATION")), ncol = 1)+
-  geom_vline(deploy, mapping = aes(xintercept = as.Date(Datetime_deployment)), linetype = "twodash", color = "red")
+  geom_vline(deploy, mapping = aes(xintercept = as.Date(Datetime_deployment_local)), linetype = "twodash", color = "red")
