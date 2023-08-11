5550cb39b7de845c7e79e6020fe6d288408393f2
leahcrowe-otago <leah.crowe@postgrad.otago.ac.nz>
2023-08-11 12:17:46 +1200

    rgdal to sf, datetime_local

--- scripts/Deployment_map.R
+++ scripts/Deployment_map.R
@@ -1,8 +1,6 @@
-library(leaflet);library(leaflet.esri);library(leaflet.extras);library(dplyr);library(rgdal);library(readxl)
+library(leaflet);library(leaflet.esri);library(leaflet.extras);library(dplyr);library(sf);library(readxl)
 
-protected_areas<-readOGR("./shapefiles", layer = "protected-areas")
-CRS.latlon<-CRS("+proj=longlat +datum=WGS84 +no_defs")
-protected_areas<-sp::spTransform(protected_areas, CRS.latlon)
+protected_areas<-sf::read_sf("C:/Users/leahm/OneDrive - University of Otago/Documents/git-otago/Fiordland_reporting/shapefiles", layer = "protected-areas") #https://data.linz.govt.nz/layer/53564-protected-areas/
 natpark<-subset(protected_areas, (section == "s.4 - National Park"))
 mpa<-subset(protected_areas, (section == "s.3 - Marine Reserve"))
 
@@ -11,8 +9,8 @@ deployments$Latitude<-round(deployments$Latitude,5)
 deployments$Longitude<-round(deployments$Longitude,5)
 
 deployments<-deployments%>%
-  filter(!is.na(Datetime_deployment))%>%
-  filter(is.na(Datetime_retrieval))%>%
+  filter(!is.na(Datetime_deployment_local))%>%
+  filter(is.na(Datetime_retrieval_local))%>%
   group_by(substr(Deployment_number,1, stringr::str_length(Deployment_number)-3))%>%
   as.data.frame()
 
@@ -22,7 +20,7 @@ deploy_leaf<-leaflet(data = deployments) %>%
   addEsriBasemapLayer(esriBasemapLayers$Oceans, autoLabels=TRUE)%>%
   addPolygons(data = mpa, color = "orange", weight = 1) %>%
   addCircleMarkers(lng = ~deployments$Longitude, lat = ~deployments$Latitude, color = ~factpal(deployments$PROJECT), stroke = FALSE, fillOpacity = 2, radius = 5,
-                   popup = ~paste(sep = "<br/>",Deployment_number,as.Date(Datetime_deployment),Recorder_type,Latitude, Longitude)) %>%
+                   popup = ~paste(sep = "<br/>",Deployment_number,as.Date(Datetime_deployment_local),Recorder_type,Latitude, Longitude)) %>%
   addLegend(pal = factpal, values = deployments$PROJECT, opacity = 1, title = "Project")%>%
   addLegend(colors = "orange", labels = "Marine Reserves")%>%
   fitBounds(min(deployments$Longitude), min(deployments$Latitude), max(deployments$Longitude), max(deployments$Latitude))
