b7e65dd0e3abb07bb1ebf7863890d690e85c7e53
leahcrowe-otago <leah.crowe@postgrad.otago.ac.nz>
2024-07-03 15:22:28 +1200

    upload occupancy model for all sites

--- scripts/occupancy.R
+++ scripts/occupancy.R
@@ -1,65 +1,153 @@
 #### Dagg comparison
 
-ch<-readRDS("./data/ch.rds")
-
 library(RPresence)
 library(jagsUI)
 library(R2OpenBUGS)
 library(dplyr)
 
-s<-nrow(ch)
-samp<-ch$samp ## 1 = ST corvariate, sampled 15 out of 30 min, 0 = continuous
-fpod<-ch$FPOD
-ST<-ch$ST
+dat = vector("list",8)
+dat[[1]]=readRDS("./data/dagg_ch.rds")
+dat[[2]] = readRDS("./data/nancy_ch.rds")
+dat[[3]] = readRDS("./data/charles_ch.rds")
+dat[[4]] = readRDS("./data/chalky_ch.rds")
+dat[[5]] = readRDS("./data/pres_ch.rds")
+dat[[6]] = readRDS("./data/dusky_ch.rds")
+dat[[7]] = readRDS("./data/mr1_ch.rds")
+dat[[8]] = readRDS("./data/mr2_ch.rds")
+names(dat) = c("dagg", "nancy", "charles","chalky","pres","dusky","mr1","mr2")
 
-h<-as.matrix(ch%>%dplyr::select(-date,-samp))
-K = 2
-h[1,2]
+ns = length(dat)
+
+s = rep(NA, ns)
+
+for(j in 1:ns){
+  s[j] = nrow(dat[[j]])
+}
+
 model<-function(){
-  
-  ## model structure
-  for (i in 1:s){
-    #logit(psi[i])<-a1+a2*samp[i]
-    z[i]~dbern(psi)
-    mu[i] <- p*z[i]
+
+## model structure
+  ## first dagg
+  for (i in 1:s[1]){
+    zd[i]~dbern(psi[1])
+    
+    logit(pd[i,1]) <- beta[1] # FPOD: not sure why I'm putting this on logit scale -- prob to be comparable to ST
+    logit(pd[i,2]) <- beta[2] + beta[3] * sampd[i] + beta[4] * analysisd[i]   
     for(j in 1:K){
-      #logit(p[i,j])<-b1+b2*fpod[i]+b3*ST[i]
-      #p.temp[i,j]<-p[i,j]*z[i]
-      h[i,j]~dbern(mu[i])
-      
-      }
+      yd[i,j]~dbern(pd[i,j]*zd[i]) 
+    }
+  }
+  
+  ## then nancy
+  for (i in 1:s[2]){
+    zn[i]~dbern(psi[2])
+    
+    logit(pn[i]) <- beta[2] + beta[3] * sampn[i]
+    yn[i]~dbern(pn[i]*zn[i]) 
+  }
+  
+  ## then charles
+  for (i in 1:s[3]){
+    zc[i]~dbern(psi[3])
+    
+    logit(pc[i]) <- beta[1]
+    yc[i]~dbern(pc[i]*zc[i]) 
+  }
+  
+  ## then chalky
+  for (i in 1:s[4]){
+    zch[i]~dbern(psi[4])
+    
+    logit(pch[i]) <- beta[2] + beta[3] * sampch[i]
+    ych[i]~dbern(pch[i]*zch[i]) 
+  }
+  
+  ## then preservation
+  for (i in 1:s[5]){
+    zp[i]~dbern(psi[5])
+    
+    logit(pp[i]) <- beta[1]
+    yp[i]~dbern(pp[i]*zp[i]) 
+  }
+  
+  ## then dusky/anchor site
+  for (i in 1:s[6]){
+    za[i]~dbern(psi[6])
+    
+    logit(pa[i]) <- beta[1]
+    ya[i]~dbern(pa[i]*za[i]) 
+  }
+  
+  ## then dusky MR-1
+  for (i in 1:s[7]){
+    zm1[i]~dbern(psi[7])
+    
+    logit(pm1[i]) <- beta[1]
+    ym1[i]~dbern(pm1[i]*zm1[i]) 
+  }
+  
+  ## then dusky MR-2
+  for (i in 1:s[8]){
+    zm2[i]~dbern(psi[8])
+    
+    logit(pm2[i]) <- beta[1]
+    ym2[i]~dbern(pm2[i]*zm2[i]) 
   }
   
   #priors
-  p~dunif(0,1)
-  psi~dunif(0,1)
+  for(j in 1:8){
+    psi[j] ~ dbeta(0.5,0.5)
+  }
   
-  #a1~dnorm(0,1)
-  #a2~dnorm(0,1)
   
-  #b1~dnorm(0,1)
-  #b2~dnorm(0,1)
-  #b3~dnorm(0,1)
+  for(j in 1:4){
+    beta[j] ~ dt(0,1,3) 
+  }
   
-  #logit(psi1[1])<-a1
-  #logit(psi1[2])<-a1+a2
-  
-  #logit(p1[1])<-b1
-  #logit(p1[2])<-b1+b2
-  #logit(p1[3])<-b1+b3
+  #transformed parameters
+  logit(pie[1])<-beta[1] # probability of FPOD
+  logit(pie[2])<-beta[2] # probability of ST, no sampling
+  logit(pie[3])<-beta[2] + beta[3] # probability of ST, sampling
+  logit(pie[4])<-beta[2] + beta[3] + beta[4] # probability of ST, sampling, handbrowse
 }
 
-write.model(model,con="dagg_model.txt") # write JAGS model code to file
-mcmc.data<-list(s=s,K=K,h=h)# samp=samp,fpod=fpod,ST=ST) # define input variables for model
-mcmc.params<-c("psi","p")
-#mcmc.params<-c("psi1","p1","a1","a2","b1","b2","b3") # define which node values to store
-mcmc.inits<-function() {list(z=rep(0.5,s))}
 
-Sys.time()
-dagg_samp <- jags(data=mcmc.data, inits = mcmc.inits, parameters.to.save=mcmc.params,
-              n.iter=11000, model.file="dagg_model.txt",n.chains=3,parallel=TRUE,verbose=TRUE,n.burnin = 1000)
-Sys.time()
+mcmc.data<-list(s=s,
+                yd = dat$dagg[,2:3],
+                yn = dat$nancy[,2],
+                yc = dat$charles[,2],
+                ych = dat$chalky[,2],
+                yp = dat$pres[,2],
+                ya = dat$dusky[,2],
+                ym1 = dat$mr1[,2],
+                ym2 = dat$mr2[,2],
+                sampd = dat$dagg$samp,
+                sampn = dat$nancy$samp,
+                sampch = dat$chalky$samp,
+                analysisd = dat$dagg$analysis,
+                analysisch = dat$chalky$analysis,
+                K=K) #samp=samp, define input variables for model
+mcmc.params<-c("psi","pie","beta")
+mcmc.inits<-function() {list(zd=rep(1,s[1]), zn = rep(1,s[2]), zc = rep(1,s[3]), zch = rep(1,s[4]), 
+                             zp = rep(1,s[5]), za = rep(1,s[6]), zm1 = rep(1,s[7]), zm2 = rep(1,s[8]))} # z has to be 0 or 1
 
-bayesplot::mcmc_trace(dagg_samp$samples)
-bayesplot::mcmc_dens(dagg_samp$samples)
-dagg_samp
+write.model(model,con="FAM_model.txt") # write JAGS model code to file
+FAM_samp <- jags(data=mcmc.data, inits = mcmc.inits, parameters.to.save=mcmc.params,
+                  n.iter=11000, model.file="FAM_model.txt",n.chains=3,parallel=TRUE,verbose=TRUE,n.burnin = 2000)
+saveRDS(FAM_samp, file = paste0("./data/FAM_samp_",Sys.Date(),".rds"))
+
+FAM_samp$samples
+FAM_samp$summary
+
+bayesplot::mcmc_trace(FAM_samp$samples)
+bayesplot::mcmc_dens(FAM_samp$samples)
+
+#matt's way below
+m1 = jags.model("FAM_model.txt", data = mcmc.data, inits = mcmc.inits, n.chains = 3, n.adapt = 1000)
+out1 = coda.samples(model = m1, variable.names = mcmc.params, n.iter = 10000)
+FAM1_df = as_draws_df(FAM_samp)
+
+mcmc_trace(out1_df)
+summary(out1_df)
+
+mean(out1_df$`beta[1]` > out1_df$`beta[2]`)
